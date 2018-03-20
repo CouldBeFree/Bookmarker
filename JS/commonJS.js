37689f413b53f338fc411c5cd3b83674aa1429ce
Sanya Zoria <propokerstar90@gmail.com>
2018-03-20 15:15:51 +0200

    Enabled bookmark list

--- JS/commonJS.js
+++ JS/commonJS.js
@@ -29,5 +29,17 @@ function fetchingBookmarks() {
 
     var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
     var bookmarksResults = document.getElementById('bookmarksResults');
-    bookmarksResults.innerHTML = 'Hello';
+    bookmarksResults.innerHTML = '';
+
+    for(var i = 0; i < bookmarks.length; i++){
+        var name = bookmarks[i].name;
+        var url = bookmarks[i].url;
+
+        bookmarksResults.innerHTML += '<div class="well">'+
+                                      '<h3>'+name+
+                                      ' <a class="btn btn-default" target="_blank" href="'+url+'">Visit</a>' +
+                                      ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" target="_blank" href="#">Delete</a>' +
+                                      '</h3>'+
+                                      '</div>';
+    }
 }
