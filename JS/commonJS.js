166292bc2157f5dd6723f6241b3feb80eea5f4a9
Sanya Zoria <propokerstar90@gmail.com>
2018-03-20 14:56:02 +0200

    Created logic for setting data to the local storage

---
+++ JS/commonJS.js
@@ -0,0 +1,33 @@
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
+
+function saveBookmark(e) {
+
+    var siteName = document.getElementById('siteName').value;
+    var siteUrl = document.getElementById('siteUrl').value;
+
+    var bookmark = {
+      name: siteName,
+      url: siteUrl
+    };
+
+    if(localStorage.getItem('bookmarks') === null){
+
+        var bookmarks = [];
+        bookmarks.push(bookmark);
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    } else {
+
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        bookmarks.push(bookmark);
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
+
+    e.preventDefault();
+}
+
+function fetchingBookmarks() {
+
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    var bookmarksResults = document.getElementById('bookmarksResults');
+    bookmarksResults.innerHTML = 'Hello';
+}
